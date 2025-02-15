80c9a8bb5372f3393d88e8a5dbd9874abca47cad
Your Name <you@example.com>
2025-02-15 19:27:28 +0800

    Add fillable properties and relationships to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,4 +9,54 @@ class Order extends Model
 {
     /** @use HasFactory<\Database\Factories\OrderFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+            'user_id',
+            'total',
+            'status',
+            'payment_method',
+            'payment_id',
+        ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function getTotalQuantityAttribute()
+    {
+        return $this->items->sum('quantity');
+    }
+
+    public function getTotalPriceAttribute()
+    {
+        return $this->items->sum('price');
+    }
+
+    public function getTotalDiscountAttribute()
+    {
+        return $this->items->sum('discount');
+    }
+
+    public function getTotalAttribute()
+    {
+        return $this->items->sum('total');
+    }
+
+    public function getStatusAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+
 }
