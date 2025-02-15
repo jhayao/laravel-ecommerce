c4c1004d2faece1efd3e65cbe80e3fa014f3a363
Your Name <you@example.com>
2025-02-15 19:23:57 +0800

    Add fillable properties and relationships to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProductFactory> */
-    use HasFactory;
+  /** @use HasFactory<\Database\Factories\ProductFactory> */
+  use HasFactory;
+
+  protected $fillable =
+    [
+      'name',
+      'price',
+      'description',
+      'sku',
+      'barcode',
+      'image',
+      'discounted_price',
+      'status',
+      'category_id'
+    ];
+
+  public function category(): BelongsTo
+  {
+    return $this->belongsTo(Category::class);
+  }
+  public function orders(): BelongsToMany
+  {
+    return $this->belongsToMany(OrderItem::class);
+  }
 }
