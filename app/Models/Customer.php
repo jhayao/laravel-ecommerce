10c8517a06ee7013974a625d7856ca2cae039d9f
Junriel Hayao <jhayao@hackazouk.com>
2025-06-03 08:52:11 +0800

    Change Customer model to extend Authenticatable for improved authentication handling

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
   /** @use HasFactory<\Database\Factories\CustomerFactory> */
   use HasFactory, HasApiTokens, Notifiable;
