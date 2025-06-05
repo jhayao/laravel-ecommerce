df2137bcd076bde5328fbec9b589053855db14b7
Junriel Hayao <jhayao@hackazouk.com>
2025-06-05 11:11:07 +0800

    test

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
 
   'onesignal' => [
     'app_id' => env('ONESIGNAL_APP_ID'),
-    'rest_api_key' => env('ONESIGNAL_REST_API_KEY'),
+    'rest_api_key' => env('ONESIGNAL_REST_API_KEY','not-set'),
     'api_url' => env('ONESIGNAL_API_URL', 'https://onesignal.com/api/v1'),
   ],
 
